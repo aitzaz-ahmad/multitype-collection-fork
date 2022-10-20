97b0ee369712d54c10022f0bc1c5b5e5029eb047
aitzaz ahmad <cptn.aitzaz@gmail.com>
2022-10-20 18:47:30 +0200

    initial commit
    adds the PoC stub developed for a multitype key-value collection.
    
    the collection stores values of different data types against string keys by capitalizing on the type-erasure technique.

---
+++ configurations.cpp
@@ -0,0 +1,43 @@
+#include "configurations.h"
+
+void configurations::insert(const property& config_property) 
+{
+    auto search = m_config_properties.find(config_property.hash_key());
+
+    if (search == m_config_properties.end()) //insert unique properties only
+    {
+        m_config_properties[config_property.hash_key()] = config_property;
+    }
+}
+
+bool configurations::exists(const std::string& section, const std::string& key) const
+{
+    auto search = m_config_properties.find(section + "_" + key);
+    return search != m_config_properties.end();
+}
+
+template<typename U>
+std::optional<U> configurations::get_value(const std::string& section,
+                                           const std::string& key) const
+{
+    if (!exists(section, key))
+    {
+        return std::optional<U>{};
+    }
+
+    const auto& config = m_config_properties.at(section + "_" + key);
+    return std::optional{ config.get_as<U>() };
+}
+
+//TODO: Move the following region into a separate cpp file (and include configurations.cpp file in it)
+#pragma region - template specializations for configuration::get_value member function
+template std::optional<int> configurations::get_value(const std::string& section,
+                                                      const std::string& key) const;
+
+template std::optional<bool> configurations::get_value(const std::string& section,
+                                                       const std::string& key) const;
+
+template std::optional<std::string> configurations::get_value(const std::string& section,
+                                                              const std::string& key) const;
+
+#pragma endregion - template specializations for configuration::get_value member function
---
+++ configurations.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <any>
+#include <string>
+#include <optional>
+#include <type_traits>
+#include <unordered_map>
+
+
+class property 
+{
+public:
+    property(const std::string& section,
+             const std::string& key,
+             const std::any& val):  m_section(section),
+                                    m_key(key),
+                                    m_value(val) {
+    }
+
+    property() = default;
+    ~property() = default;
+    property(const property&) = default;
+    property& operator=(const property&) = default;
+    property(property&&) = default;
+    property& operator=(property&&) = default;
+
+    inline std::string hash_key() const {
+
+        return m_section + "_" + m_key;
+    }
+
+    template<class T>
+    std::optional<T> get_as() const {
+
+        if (!m_value.has_value())
+            return std::optional<T>{};
+
+        if(m_value.type() != typeid(T))
+            return std::optional<T>{};
+        
+        return std::optional{ std::any_cast<T>(m_value) };
+    }
+
+private:
+
+    std::string m_section;
+    std::string m_key;
+    std::any    m_value;
+};
+
+using server_configs = std::unordered_map<std::string, property>;
+
+class configurations {
+public:
+    void insert(const property& config_property);
+    bool exists(const std::string& section, const std::string& key) const;
+    
+    template<class U>
+    std::optional<U> get_value(const std::string& section,
+                               const std::string& key) const;
+
+    /**
+    * <-------  TODO  ------->
+    * 
+    * @author:  Aitzaz Ahmad
+    * @date:    25.07.2022
+    * 
+    * @remark:  Consider adding an overload for the get_value function template
+    *           which accepts a 'const property&' as an input.
+    */
+
+private:
+    server_configs m_config_properties;
+};
